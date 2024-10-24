cfcc65ac769dd5b2cb3d3e5bb253a72c37748d44
shasahasr <shasahasr@gmail.com>
2024-10-23 19:31:46 -0500

    gh-pages maybe????

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { base } from "$app/paths";
+</script>
+
+<nav>
+  <a href="{base}/">Home</a>
+</nav>
